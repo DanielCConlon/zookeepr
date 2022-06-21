4e19d2acd77cf673560fa9568144de7dad4b4f7b
Daniel Conlon <danielcconlon@gmail.com>
2022-06-21 17:11:00 -0400

    add the ability to send data to the api

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # zookeepr
+
+# 11.1
+
+- Set up and run a Node.js web server using Express.js
+
+- Handled two types of GET requests using parameters
+
+- Created a production-ready application and deployed it to Heroku
+
+# 11.2
+
+- Create a different type of API endpoint to accept incoming (often called POST) data from a client's request.
+
+- Implement functionality called middleware so our server can understand the type of data we are looking to post.
+
+- Use a tool called Insomnia to test POST requests while we wait for a finished front end.
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { stringify } = require('querystring');
 
 const { animals } = require('./data/animals.json');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended:true }));
+// parse incomgin json data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -56,6 +64,40 @@ function findById(id, animalsArray) {
     return result;
 };
 
+function createNewAnimal(body, animalsArray) {
+    // our function's main code will go here!
+    const animal = body;
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    // return finished code to post route for response
+    return animal;
+};
+
+function validateAnimal(animal) {
+    if(!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+
+    if(!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+
+    if(!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+
+    return true;
+};
+
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -77,6 +119,20 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
 
+});
+
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if(!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    }
+    else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
 });
 
 
